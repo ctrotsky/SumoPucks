b6cf7bd8e02a1a2f662ed8f4aa0cbf0584577036
ctrotsky <ctrotter136@gmail.com>
2016-10-22 00:06:53 -0700

    2D project

---
+++ SumoPucks/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		
+		Vector3 aim = new Vector3(h, 0.0f, v);
+
+	}
+
+	void Fling () {
+		
+	}
+}
