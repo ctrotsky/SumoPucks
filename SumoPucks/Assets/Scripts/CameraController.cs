d0aa2c71657a95ae5edc44b68a3a5609dbd1fbd0
Stephen7137 <scjackson@cpp.edu>
2016-10-22 07:03:29 -0700

    Gui and sprites

--- SumoPucks/Assets/Scripts/CameraController.cs
+++ SumoPucks/Assets/Scripts/CameraController.cs
@@ -16,6 +16,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("info");
         setCameraPos();
         setCameraSize();
     }
