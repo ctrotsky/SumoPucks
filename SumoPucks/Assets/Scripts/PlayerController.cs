5d6efee3d8841f5ddb5f0d3dc2a8a9a4e6a0678c
ctrotsky <ctrotter136@gmail.com>
2016-10-22 02:46:02 -0700

    Modified player movement. Began work on multiple player inputs. Added aimer on top of player.

--- SumoPucks/Assets/Scripts/PlayerController.cs
+++ SumoPucks/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour {
 	private float Flickpower;
 	public float maxFlickPower = 50;
 	public float friction = 1;
+	public float chargeSpeed = 1;
+	public float chargeCooldown; //unimplemented
+	public int playerNum;
+	public GameObject aimer;
 	
 	// Use this for initialization
 	void Start () {
@@ -17,25 +21,36 @@ public class PlayerController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton("Fire1")){
-			ChargeFlick();
+		Vector2 aim = getAim();
+
+		if (Input.GetButton("Player" + playerNum + "A")){
+			ChargeFlick(aim);
 		}
-		if (Input.GetButtonUp("Fire1")){
-			Flick();
+		if (Input.GetButtonUp("Player" + playerNum + "A")){
+			Flick(aim);
 		}
 	}
 
-	void ChargeFlick() {
-		Mathf.Clamp(Flickpower++, 0, maxFlickPower);
+	void ChargeFlick(Vector2 aim) {
+		if (Flickpower < maxFlickPower){
+			Flickpower+= chargeSpeed;
+		}
+		var angle = Mathf.Atan2(aim.y, aim.x) * Mathf.Rad2Deg + 270; //added degrees at end will change depending on which way sprite faces
+ 		aimer.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 
-	void Flick () {
-		float h = Input.GetAxis("Horizontal");
-		float v = Input.GetAxis("Vertical");
+	void Flick (Vector2 aim) {
+		print("aim: " + aim);
 
-		Vector2 aim = new Vector2(h, v).normalized * Flickpower;
-
-		rb.AddForce(aim);
+		rb.AddForce(aim * Flickpower * -1);
 		Flickpower = 0;
+	}
+
+	Vector2 getAim () {
+		float h = Input.GetAxis("Player" + playerNum + "Horizontal");
+		float v = Input.GetAxis("Player" + playerNum + "Vertical");
+
+		Vector2 aim = new Vector2(h, v);
+		return aim;
 	}
 }
